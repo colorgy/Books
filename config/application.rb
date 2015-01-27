2a677edab1ab954e3a570c8f9129c338522a983f
Neson <neson@dex.tw>
2015-01-27 17:33:22 +0800

    rename app

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Core
+module Books
   class Application < Rails::Application
     Dotenv::Railtie.load
     # Settings in config/environments/* take precedence over those specified here.
