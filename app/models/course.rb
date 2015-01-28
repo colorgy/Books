4da3c9b72c923349497f4dc876fb752d4c00473b
Yukai Huang <yukaihuang1993@hotmail.com>
2015-01-28 23:55:46 +0800

    fix course have book_data

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-  belongs_to :book,
+  belongs_to :book_data,
               class_name: :BookData, foreign_key: :book_isbn, primary_key: :isbn
 
   scope :confirmed, -> { where(confirmed: true) }
