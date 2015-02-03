5f0ece0151c38abb16b2b2adca8b3392339d1743
Neson <neson@dex.tw>
2015-02-03 12:01:43 +0800

    load dotenv only if defined
    #setup

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Books
   class Application < Rails::Application
-    Dotenv::Railtie.load
+    Dotenv::Railtie.load if defined? Dotenv::Railtie
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
