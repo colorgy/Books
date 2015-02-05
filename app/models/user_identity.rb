07b7a1fb60017f8ff777f54ed81ceb0bfc3bcb8b
Neson <neson@dex.tw>
2015-02-05 10:59:59 +0800

    consider prof/lecturer identities with no name as invalid prof/lecturer identity

--- app/models/user_identity.rb
+++ app/models/user_identity.rb
@@ -2,7 +2,7 @@ class UserIdentity < ActiveRecord::Base
   extend FriendlyId
   friendly_id :sid, use: :finders
 
-  scope :lecturer, -> { where(identity: ['lecturer', 'professor']) }
+  scope :lecturer, -> { where(identity: ['lecturer', 'professor']).where.not(name: nil).where.not(name: '') }
 
   belongs_to :user
   has_many :courses, ->(o) { where "courses.organization_code = ?", o.organization_code },
--- spec/models/user_identity_spec.rb
+++ spec/models/user_identity_spec.rb
@@ -11,14 +11,17 @@ RSpec.describe UserIdentity, :type => :model do
 
     expect(user_identity.courses).to include(course_in_org)
     expect(user_identity.courses).not_to include(course_in_other_org)
-
   end
 
   describe ".lecturer" do
-    it "scopes the professor and lecturer" do
-      create_list(:user_identity, 30)
+    it "scopes the professor and lecturer with name" do
+      create_list(:user_identity, 100)
+      create(:user_identity, identity: 'professor', name: '')
+      create(:user_identity, identity: 'professor', name: nil)
+
       UserIdentity.lecturer.each do |ui|
         expect(ui.identity).to satisfy { |i| %w(professor lecturer).include?(i) }
+        expect(ui.name).not_to be_blank
       end
     end
   end
