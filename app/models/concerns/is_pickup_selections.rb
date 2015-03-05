fa4e484403f5c3d2a7a140345b8ef57b88790f4b
Neson <neson@dex.tw>
2015-03-05 16:19:36 +0800

    let group pickup selections remain the same

--- app/models/concerns/is_pickup_selections.rb
+++ app/models/concerns/is_pickup_selections.rb
@@ -8,8 +8,12 @@ module IsPickupSelections
   module ClassMethods
 
     def selections_for(org_code, batch = BatchCodeService.current_batch)
+      if org_code.is_a?(Group)
+        group = org_code
+        org_code = group.organization_code
+        batch = group.batch
+      end
       where('(organization_code = ? OR organization_code IS NULL) AND (batch = ? OR batch IS NULL)', org_code, batch).map(&:selection)
     end
-
   end
 end
