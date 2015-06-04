9425e8ced0c571693a029a7f9ad1c6993b8ad538
Neson <neson@dex.tw>
2015-06-04 17:26:02 +0800

    redirect back after login for SCP

--- app/controllers/supplier/sessions_controller.rb
+++ app/controllers/supplier/sessions_controller.rb
@@ -1,8 +1,8 @@
 class Supplier::SessionsController < Devise::SessionsController
   layout 'supplier'
 
-  def after_sign_in_path_for(_)
-    supplier_control_panel_dashboard_path
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || supplier_control_panel_dashboard_path
   end
 
   def after_sign_out_path_for(_)
