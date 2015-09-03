1e8c69d45e47a18f6d98160ebd6255afec60a577
Yukai Huang <yukaihuang1993@hotmail.com>
2015-09-03 12:22:28 +0800

    made taiwan_mobile_img factory passed
    有附上參照網址，妹寺大好。

--- spec/factories/taiwan_mobile_imgs.rb
+++ spec/factories/taiwan_mobile_imgs.rb
@@ -1,7 +1,9 @@
+# http://stackoverflow.com/questions/24355501/using-factorygirls-attributes-for-with-paperclip-attachment
+
 FactoryGirl.define do
   factory :taiwan_mobile_img do
-    image_url "MyString"
-user nil
+    image_url Rack::Test::UploadedFile.new(Rails.root.join('spec', 'images', 'test.png'), "image/png")
+    user nil
   end
 
 end
