2df1d79dec1db50c56401aac7b8b131970307478
Yukai Huang <yukaihuang1993@hotmail.com>
2015-09-03 19:52:55 +0800

    simple dashbard / course_book update

--- app/admin/course_book.rb
+++ app/admin/course_book.rb
@@ -2,6 +2,12 @@ ActiveAdmin.register CourseBook do
 
   permit_params :course_ucode, :book_isbn, :book_known, :updated_by, :confirmed, :created_at, :updated_at, :book_required, :updater_code, :locked, :updater_type
 
+  controller do
+    def scoped_collection
+      super.includes :course, :book_data
+    end
+  end
+
   filter :course_ucode
   filter :book_isbn
   filter :book_known
@@ -13,6 +19,24 @@ ActiveAdmin.register CourseBook do
   filter :updater_code
   filter :locked
   filter :updater_type
+
+  index do
+    selectable_column
+
+    column('Course Ucode') {|cb| cb.course.present? ? a(cb.course_ucode, href: admin_course_path(cb.course)) : cb.course_ucode }
+    column('Course Name')  {|cb| cb.course.present? ? a(cb.course.name, href: admin_course_path(cb.course)) : cb.course_ucode }
+    column('Book Isbn') {|cb| cb.book_data.present? ? a(cb.book_isbn, href: admin_book_data_path(cb.book_data)) : cb.book_isbn }
+    column('Book Name') {|cb| cb.book_data.present? ? a(cb.book_data.name, href: admin_book_data_path(cb.book_data)) : cb.book_isbn }
+    column(:updated_by)
+    column(:updater_type)
+    column(:updater_code)
+    column(:book_required)
+    column(:book_known)
+    column(:locked)
+    column(:confirmed)
+
+    actions
+  end
 
   form do |f|
     f.inputs '關聯資料' do
