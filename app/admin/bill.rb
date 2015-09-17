4f5ee2d5d6ebaf10c0b1e59c504974c1dcd3b3e4
Yukai Huang <yukaihuang1993@hotmail.com>
2015-09-17 12:09:38 +0800

    add download all

--- app/admin/bill.rb
+++ app/admin/bill.rb
@@ -152,6 +152,10 @@ ActiveAdmin.register Bill do
     download_orders(Order.order('orders.course_ucode').by_supplier_code.where('orders.created_at > ?', Date.new(2015, 8, 1)).where(state: :ready))
   end
 
+  collection_action :download_all_packing_list, :method => :get do
+    download_orders(Order.order('orders.course_ucode').by_supplier_code.where('orders.created_at > ?', Date.new(2015, 8, 1)).where(order_date: nil).where('state = ? OR state = ?', 'ready', 'new'))
+  end
+
   collection_action :download_cave_base, :method => :get do
     download_orders(Order.order('orders.course_ucode').by_supplier_code.where('orders.created_at > ?', Date.new(2015, 8, 1)).where('state = ?', 'ready').where(order_date: nil))
   end
@@ -170,6 +174,10 @@ ActiveAdmin.register Bill do
 
   action_item only: :index do
     link_to('出貨表單（全部）', params.merge(action: :download_packing_list) )
+  end
+
+  action_item only: :index do
+    link_to('出貨表單（所有的入門的）', params.merge(action: :download_all_packing_list) )
   end
 
   action_item only: :index do
